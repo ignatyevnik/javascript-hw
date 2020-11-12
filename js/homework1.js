795d98e956087268e7ccad69eb97e1e7938f5b34
Ignatyev Nikita <necrevan@gmail.com>
2020-11-12 16:12:50 +0300

    js homework 1

---
+++ js/homework1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Задание №1
+let a = 4, b = 5, c = 3;
+console.log('Высота a = ' + a);
+console.log('Длина b = ' + b);
+console.log('Ширина c = ' + c);
+console.log('Площадь равна ' + a * b * c);
+console.log();
+
+// Задание №2
+let n = 76;
+console.log('Значение n равно ' + n + '.' + ' Сумма чисел равна ' + [+'7'+6]);
+console.log();
+
+// Задание №3
+let numFirst = 4, numSecond = 6;
+console.log('Было ', numFirst, numSecond);
+numFirst += 7;
+numSecond += 4;
+console.log('Стало ', numFirst, numSecond);
